9f9127481cfc6f7c41a922af3ea1ec1623eebb25
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2021-10-28 01:21:49 +0000

    Update Jest and Modified Tests To Pass

---
+++ pages/__tests__/index.test.tsx
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from "@testing-library/react";
+import Index from "../index";
+
+describe("Index", () => {
+  it("renders main text", () => {
+    render(<Index />);
+
+    const paragraph = screen.getByText(
+      /Our group is for anybody who wants to talk about/i
+    );
+
+    expect(paragraph).toBeInTheDocument();
+  });
+});
